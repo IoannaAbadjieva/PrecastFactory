8d5c0d5ff016052d9735fddd1ac09c6b226a6f18
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-29 19:37:54 +0200

    Add project files.

---
+++ PrecastFactorySystem/Data/PrecastFactorySystemDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace PrecastFactorySystem.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class PrecastFactorySystemDbContext : IdentityDbContext
+    {
+        public PrecastFactorySystemDbContext(DbContextOptions<PrecastFactorySystemDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
