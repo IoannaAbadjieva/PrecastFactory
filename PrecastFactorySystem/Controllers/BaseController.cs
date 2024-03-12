9048a245f1b47ba37453bc9c3b24e5a1f46c0d38
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-03-13 00:16:09 +0200

    Unnessesary usings removed from BaseController

--- PrecastFactorySystem/Controllers/BaseController.cs
+++ PrecastFactorySystem/Controllers/BaseController.cs
@@ -1,15 +1,9 @@
 ﻿namespace PrecastFactorySystem.Controllers
 {
-	using System.Linq.Expressions;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
 
-	using Microsoft.AspNetCore.Authorization;
-	using Microsoft.AspNetCore.Mvc;
-
-	using PrecastFactorySystem.Core.Contracts;
-	using PrecastFactorySystem.Core.Models.Base;
-	using PrecastFactorySystem.Data.Models;
-
-	[Authorize]
+    [Authorize]
 	public  class BaseController : Controller
 	{
 	
