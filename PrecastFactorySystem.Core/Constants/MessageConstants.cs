bfeacec67e71e4dd2530c38230223ba945be2fdf
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-04-01 23:15:03 +0300

    Custom onDelete error added

---
+++ PrecastFactorySystem.Core/Constants/MessageConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace PrecastFactorySystem.Core.Constants
+{
+	public class MessageConstants
+	{
+		public const string DeleteProjectErrorMessage = "You can not delete this projectt, it has precast in production";
+
+		public const string DeletePrecastErrorMessage = "You can not delete this precast, it is in production";
+
+		public const string DeleteDelivererErrorMessage = "You can not delete this deliverer, he has orders in production";
+
+		public const string InvalidPrecastCountErrorMessage = "You can not change precast count to less than {0}";
+
+	}
+}
