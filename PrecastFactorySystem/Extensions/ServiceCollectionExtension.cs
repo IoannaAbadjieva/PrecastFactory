ef0a28619b5b02358be623851dd0d1ffd32e0b89
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-04-01 23:29:48 +0300

    DelivererService added to ServiceCollectionExtension

--- PrecastFactorySystem/Extensions/ServiceCollectionExtension.cs
+++ PrecastFactorySystem/Extensions/ServiceCollectionExtension.cs
@@ -1,41 +1,44 @@
-﻿namespace Microsoft.Extensions.DependencyInjection;
-
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-
-using PrecastFactorySystem.Core.Contracts;
-using PrecastFactorySystem.Core.Services;
-using PrecastFactorySystem.Data;
-using PrecastFactorySystem.Infrastructure.Data.Common;
-
-public static class ServiceCollectionExtension
+﻿namespace Microsoft.Extensions.DependencyInjection
 {
-	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	using AspNetCore.Identity;
+	using EntityFrameworkCore;
+
+	using PrecastFactorySystem.Core.Contracts;
+	using PrecastFactorySystem.Core.Services;
+	using PrecastFactorySystem.Infrastructure.Data;
+	using PrecastFactorySystem.Infrastructure.Data.Common;
+
+	public static class ServiceCollectionExtension
 	{
-		services.AddScoped<IRepository, Repository>();
-		services.AddScoped<IProjectService, ProjectService>();
-		services.AddScoped<IPrecastService, PrecastService>();
-		return services;
-	}
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+		{
+			services.AddScoped<IRepository, Repository>();
+			services.AddScoped<IProjectService, ProjectService>();
+			services.AddScoped<IPrecastService, PrecastService>();
+			services.AddScoped<IReinforceService, ReinforceService>();
+			services.AddScoped<IDelivererService, DelivererService>();
+			return services;
+		}
 
-	public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
-	{
-		var connectionString = configuration.GetConnectionString("DefaultConnection");
+		public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
+		{
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-		services.AddDbContext<PrecastFactoryDbContext>(options =>
-			options.UseSqlServer(connectionString));
+			services.AddDbContext<PrecastFactoryDbContext>(options =>
+				options.UseSqlServer(connectionString));
 
-		services.AddDatabaseDeveloperPageExceptionFilter();
+			services.AddDatabaseDeveloperPageExceptionFilter();
 
-		return services;
-	}
+			return services;
+		}
 
-	public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration configuration)
-	{
-		services
-			.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-			.AddEntityFrameworkStores<PrecastFactoryDbContext>();
+		public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration configuration)
+		{
+			services
+				.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<PrecastFactoryDbContext>();
 
-		return services;
+			return services;
+		}
 	}
 }
