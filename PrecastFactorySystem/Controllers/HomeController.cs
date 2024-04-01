0c53e3ebcfd8d21df3ec71db4566446681780140
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-04-01 23:20:17 +0300

    Custom error pages added in Home controller

--- PrecastFactorySystem/Controllers/HomeController.cs
+++ PrecastFactorySystem/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿namespace PrecastFactorySystem.Controllers
 {
-	using System.Diagnostics;
-
 	using Microsoft.AspNetCore.Authorization;
 	using Microsoft.AspNetCore.Mvc;
-
-	using Models;
 
 	public class HomeController : Controller
 	{
@@ -25,9 +21,24 @@
 
 		[AllowAnonymous]
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
+		public IActionResult Error(int statusCode)
 		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+			if (statusCode == 400)
+			{
+				return View("NotFound");
+			}
+
+			if (statusCode == 401)
+			{
+				return View("UnAuthorized");
+			}
+
+			if (statusCode == 404)
+			{
+				return View("BadRequest");
+			}
+
+			return View();
 		}
 	}
 }
