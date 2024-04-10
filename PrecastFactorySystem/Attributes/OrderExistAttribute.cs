98f44b0655a1eacde28b5507774bc982b67ac45b
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-04-10 18:24:00 +0300

    OrderExistFilterAttribute added

---
+++ PrecastFactorySystem/Attributes/OrderExistAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace PrecastFactorySystem.Attributes
+{
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Mvc.Filters;
+
+	using PrecastFactorySystem.Core.Contracts;
+
+	public class OrderExistAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+
+			IOrderService? orderService =
+				context.HttpContext.RequestServices.GetService<IOrderService>();
+
+			if (orderService == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+
+
+			if (orderService != null
+				&& !orderService.IsOrderExistAsync((int)(context.ActionArguments["id"] ?? 0)).Result)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+			}
+		}
+	}
+}
