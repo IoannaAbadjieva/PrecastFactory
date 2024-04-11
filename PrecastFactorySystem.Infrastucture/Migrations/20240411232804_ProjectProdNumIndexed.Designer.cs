3db168aeca8844aba842d40a6699d8ef1adacb80
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-04-12 02:29:37 +0300

    Project  property ProdNumber indexed

--- PrecastFactorySystem.Infrastucture/Migrations/20240411232804_ProjectProdNumIndexed.Designer.cs
+++ PrecastFactorySystem.Infrastucture/Migrations/20240411232804_ProjectProdNumIndexed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrecastFactorySystem.Infrastructure.Data;
 
@@ -11,9 +12,10 @@ using PrecastFactorySystem.Infrastructure.Data;
 namespace PrecastFactorySystem.Data.Migrations
 {
     [DbContext(typeof(PrecastFactoryDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240411232804_ProjectProdNumIndexed")]
+    partial class ProjectProdNumIndexed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PrecastFactorySystem.Infrastucture/Migrations/20240411232804_ProjectProdNumIndexed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrecastFactorySystem.Data.Migrations
+{
+    public partial class ProjectProdNumIndexed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ProdNumber",
+                table: "Projects",
+                column: "ProdNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_ProdNumber",
+                table: "Projects");
+        }
+    }
+}
