2f479d8fb6d106dea0f1422c44b26d8f19a61d68
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-04-21 16:29:55 +0300

    Precast added to admin area

---
+++ PrecastFactorySystem/Areas/Admin/Controllers/PrecastController.cs
@@ -0,0 +1,57 @@
+﻿namespace PrecastFactorySystem.Areas.Admin.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+
+	using PrecastFactorySystem.Attributes;
+	using PrecastFactorySystem.Core.Contracts;
+	using PrecastFactorySystem.Core.Models.Precast;
+	using PrecastFactorySystem.Core.Models.Project;
+	using PrecastFactorySystem.Infrastructure.Data.Models;
+
+	public class PrecastController : AdminBaseController
+	{
+		private readonly IPrecastService precastService;
+		private readonly IBaseServise baseService;
+
+		public PrecastController(IPrecastService _precastService,
+						IBaseServise _baseService)
+		{
+			precastService = _precastService;
+			baseService = _baseService;
+		}
+		public async Task<IActionResult> All([FromQuery] AllPrecastQueryModel model)
+		{
+			var precasts = await precastService.GetAllPrecastAsync(
+				model.SearchTerm,
+				model.ProjectId,
+				model.PrecastTypeId,
+			model.Sorting,
+			model.CurrentPage,
+				AllPrecastQueryModel.PrecastsPerPage);
+
+			model.Projects = await baseService.GetBaseEntityDataAsync<Project>();
+			model.PrecastTypes = await baseService.GetBaseEntityDataAsync<PrecastType>();
+			model.Precast = precasts.Precasts;
+			model.TotalPrecast = precasts.TotalPrecast;
+
+			return View(model);
+		}
+
+
+		[PrecastExists]
+		[HttpGet]
+		public async Task<IActionResult> Delete(int id)
+		{
+			var model = await baseService.GetEntityBaseInfoAsync<Precast>(id);
+			return View(model);
+		}
+
+		[PrecastExists]
+		[HttpPost]
+		public async Task<IActionResult> Delete(int id, ProjectInfoViewModel model)
+		{
+			await baseService.DeleteEntityAsync<Precast>(id);
+			return RedirectToAction(nameof(All));
+		}
+	}
+}
